375c49ff46fcb3082a58394d279bc568885819d6
harshk014 <harshkanani014@gmail.com>
2023-05-08 00:22:05 +0530

    test cn webhook

--- first_app/views.py
+++ first_app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World! This is CN Django Test. will be rocking soon in industry.')
+    return HttpResponse('Hello World! This is CN Django Test. will be rocking soon in industry.By - Harsh Kanani')
