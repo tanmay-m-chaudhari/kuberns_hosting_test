d9ad3575b26dc8fd8377ef2f62583c6db057c5e5
harshkanani014 <harshkanani100@gmail.com>
2023-05-25 22:51:27 +0530

    content update

--- first_app/views.py
+++ first_app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Har Har Mahadev.....Hello World! This is CN Django Test. will be rocking soon in industry.By - Harsh Kanani :aaaaaa')
+    return HttpResponse('Har Har Mahadev.....Hello World! This is CN Django Test.By - Harsh Kanani ')
