72e209e3bbdb758f324477a55183208a10461010
harshkanani014 <harshkanani100@gmail.com>
2023-06-17 00:12:57 +0530

    webhook

--- first_app/views.py
+++ first_app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Har Har Mahadev.....Hello World! This is CN Django Test.By - Harsh Kanani .....web hooka')
+    return HttpResponse('Har Har Mahadev.....Hello World! This is CN Django Test.By - Harsh Kanani .....web hooks testing by harsh')
