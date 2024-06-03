b912753a04c765372be5d34e8e0a84460b3df11c
Jevin Vekaria <jevin925@gmail.com>
2024-06-03 13:05:06 +0530

    test

--- first_app/tasks.py
+++
@@ -1,7 +0,0 @@
-
-from cn_django_test.celery import app
-
-@app.task(bind=True)
-def test_task(self):
-    print("CN testing background task")
-    return {"status": "success", "message": "CN testing background task"}
--- first_app/views.py
+++ first_app/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .tasks import test_task
-from decouple import config
+
 # Create your views here.
 
 def index(request):
-    test_task.delay()
 
     return HttpResponse(f'HHM.....Hello World! This is CN Django Test.By - Harsh Kanani aa.....web hooks test harsh kanani webhook testing 1234 ')
