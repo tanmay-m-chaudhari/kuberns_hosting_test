85eed42e67c6df0f84ccd7309a091d6db6450c6f
Jevin Vekaria <jevin925@gmail.com>
2024-06-16 01:34:26 +0530

    ram increase

--- first_app/views.py
+++ first_app/views.py
@@ -3,9 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+from django.http import HttpResponse
+
 def index(request):
+    # Create a large list to increase RAM usage
+    large_list = []
+    for i in range(100000):
+        large_list.append("hello world" * 100)  # Create a large string and append to the list
 
-    for i in range(1, 100000):
-        print("hello world")
-
-    return HttpResponse(f'HHM.....Hello World! This is CN Django Test.By - Harsh Kanani aa.....web hooks test harsh kanani webhook testing 123456789 hello hi ')
+    # Just to ensure the list is being used and not optimized away
+    total_length = sum(len(s) for s in large_list)
+    
+    return HttpResponse(f'HHM.....Hello World! This is CN Django Test.By - Harsh Kanani aa.....web hooks test harsh kanani webhook testing 123456789 hello hi. Total length of strings: {total_length}')
