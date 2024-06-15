cf363eeeb4ad01dc0ccf4288b381069accdfdbd3
Jevin Vekaria <jevin925@gmail.com>
2024-06-16 01:40:14 +0530

    dummy

--- first_app/views.py
+++ first_app/views.py
@@ -14,4 +14,5 @@ def index(request):
     # Just to ensure the list is being used and not optimized away
     total_length = sum(len(s) for s in large_list)
     
+    
     return HttpResponse(f'HHM.....Hello World! This is CN Django Test.By - Harsh Kanani aa.....web hooks test harsh kanani webhook testing 123456789 hello hi. Total length of strings: {total_length}')
