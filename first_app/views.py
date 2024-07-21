9ff2253b2262b8f26ec14ba0906c887ed5f092cd
Jevin Vekaria <jevin925@gmail.com>
2024-07-22 00:55:18 +0530

    motto removed

--- first_app/views.py
+++ first_app/views.py
@@ -13,4 +13,4 @@ def index(request):
 
     total_length = sum(len(s) for s in large_list)
 
-    return HttpResponse(f'HHM.....Hello World! This is CN Django Test.By - Harsh Kanani aa.....web hooks test harsh kanani webhook testing 123456789 hello motto. Total length of strings: {total_length}')
+    return HttpResponse(f'HHM.....Hello World! This is CN Django Test.By - Harsh Kanani aa.....web hooks test harsh kanani webhook testing 123456789 hello. Total length of strings: {total_length}')
