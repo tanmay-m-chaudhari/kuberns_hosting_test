47797af62e7426b75a9b18191417c5a470f53ac7
TANMAY CHAUDHARI <113274885+tanmay-m-chaudhari@users.noreply.github.com>
2024-12-23 09:26:12 +0530

    Update views.py

--- first_app/views.py
+++ first_app/views.py
@@ -12,5 +12,13 @@ def index(request):
         large_list.append("hello world" * 1000)  # Create a large string and append to the list
 
     total_length = sum(len(s) for s in large_list)
-
     return HttpResponse(f'HHM.....Hello World! This is CN Django Test.By - Harsh Kanani aa.....web hooks test harsh kanani webhook testing 123456789 hello jevin. Total length of strings: {total_length}')
+
+def index1(request):
+    s = [1,2,3,4,5]
+    print(s)
+
+def index2(request):
+    import numpy
+    import pandas
+    print('imported libraries')
