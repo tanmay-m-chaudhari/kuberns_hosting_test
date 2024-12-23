db3423316c2e1d8c5b9738b4ebe44ea3039ba2ba
TANMAY CHAUDHARI <113274885+tanmay-m-chaudhari@users.noreply.github.com>
2024-12-23 09:27:05 +0530

    Update urls.py

--- first_app/urls.py
+++ first_app/urls.py
@@ -4,4 +4,6 @@ from first_app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/index1', views.index1, name='index1'),
+    path('/index2', views.index2, name='index2'),
 ]
